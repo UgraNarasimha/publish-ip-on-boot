21cf6bf527c6c782f520490c6cdb0eac9384d9d7
Supreeth Raghuprakash <arrsupreeth@gmail.com>
2019-08-03 13:50:06 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install mosquitto-clients -y
-wget https://raw.githubusercontent.com/eifinger/publish-ip-on-boot/master/publish-ip.sh
-wget https://raw.githubusercontent.com/eifinger/publish-ip-on-boot/master/publiship.service
+#sudo apt-get update
+#sudo apt-get install mosquitto-clients -y
+
+sudo cp publish-ip.sh /home/pi/
 chmod +x /home/pi/publish-ip.sh
-sudo mv /home/pi/publiship.service /etc/systemd/system/publiship.service
+
+sudo cp publiship.service /etc/systemd/system/publiship.service
 sudo systemctl enable publiship.service
 sudo systemctl start publiship.service
